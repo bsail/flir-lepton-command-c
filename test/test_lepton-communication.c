fd9a312909d3c1e2d0f86c469f432e7bee51c4a9
Nick Nerovny <nikolay.nerovniy@gmail.com>
2018-07-11 21:50:10 +0300

    Added example test for sendCommand_raw()

--- test/test_lepton-communication.c
+++ test/test_lepton-communication.c
@@ -10,7 +10,14 @@ void tearDown(void)
 {
 }
 
-void test_lepton_communication_NeedToImplement(void)
+void test_sendCommand_raw_should_work(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement lepton-communication");
+  struct lepton_communication comm;
+  uint16_t code = 0xAB;
+
+  waitCommandBegin_ExpectAndReturn(LEPFLIR_GEN_CMD_TIMEOUT,&comm,1);
+  writeCmdRegister_ExpectAndReturn(code, NULL, 0,&comm,0);
+  waitCommandFinish_ExpectAndReturn(LEPFLIR_GEN_CMD_TIMEOUT,&comm,0);
+
+  sendCommand_raw(0xAB,&comm);
 }
