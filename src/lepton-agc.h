7f9ccca1fd3f5c405eaa1567a31c1147f15b9f61
Nick Nerovny <nikolay.nerovniy@gmail.com>
2018-07-14 21:46:40 +0300

    Added null driver tests for lepton-agc.c

--- src/lepton-agc.h
+++ src/lepton-agc.h
@@ -19,6 +19,50 @@ extern "C" {
 
 #endif
 
+#ifdef TEST
+  void setAGCEnabled(struct lepton_driver *driver, uint8_t enabled);
+  uint8_t getAGCEnabled(struct lepton_driver *driver);
+  void setAGCPolicy(struct lepton_driver *driver, LEP_AGC_POLICY policy);
+  LEP_AGC_POLICY getAGCPolicy(struct lepton_driver *driver);
+  void setHEQScaleFactor(struct lepton_driver *driver,
+                         LEP_AGC_HEQ_SCALE_FACTOR factor);
+  LEP_AGC_HEQ_SCALE_FACTOR getHEQScaleFactor(struct lepton_driver *driver);
+  void setAGCCalcEnabled(struct lepton_driver *driver, uint8_t enabled);
+  uint8_t getAGCCalcEnabled(struct lepton_driver *driver);
+  void setHistogramRegion(struct lepton_driver *driver,
+                          LEP_AGC_HISTOGRAM_ROI * region);
+  void getHistogramRegion(struct lepton_driver *driver,
+                          LEP_AGC_HISTOGRAM_ROI * region);
+  void getHistogramStatistics(struct lepton_driver *driver,
+                              LEP_AGC_HISTOGRAM_STATISTICS * statistics);
+  void setHistogramClipPercent(struct lepton_driver *driver, uint16_t percent);
+  uint16_t getHistogramClipPercent(struct lepton_driver *driver);
+  void setHistogramTailSize(struct lepton_driver *driver, uint16_t size);
+  uint16_t getHistogramTailSize(struct lepton_driver *driver);
+  void setLinearMaxGain(struct lepton_driver *driver, uint16_t gain);
+  uint16_t getLinearMaxGain(struct lepton_driver *driver);
+  void setLinearMidpoint(struct lepton_driver *driver, uint16_t midpoint);
+  uint16_t getLinearMidpoint(struct lepton_driver *driver);
+  void setLinearDampeningFactor(struct lepton_driver *driver, uint16_t factor);
+  uint16_t getLinearDampeningFactor(struct lepton_driver *driver);
+  void setHEQDampeningFactor(struct lepton_driver *driver, uint16_t factor);
+  uint16_t getHEQDampeningFactor(struct lepton_driver *driver);
+  void setHEQMaxGain(struct lepton_driver *driver, uint16_t gain);
+  uint16_t getHEQMaxGain(struct lepton_driver *driver);
+  void setHEQClipLimitHigh(struct lepton_driver *driver, uint16_t limit);
+  uint16_t getHEQClipLimitHigh(struct lepton_driver *driver);
+  void setHEQClipLimitLow(struct lepton_driver *driver, uint16_t limit);
+  uint16_t getHEQClipLimitLow(struct lepton_driver *driver);
+  void setHEQBinExtension(struct lepton_driver *driver, uint16_t extension);
+  uint16_t getHEQBinExtension(struct lepton_driver *driver);
+  void setHEQMidpoint(struct lepton_driver *driver, uint16_t midpoint);
+  uint16_t getHEQMidpoint(struct lepton_driver *driver);
+  void setHEQEmptyCounts(struct lepton_driver *driver, uint16_t counts);
+  uint16_t getHEQEmptyCounts(struct lepton_driver *driver);
+  void setHEQNormalizationFactor(struct lepton_driver *driver, uint16_t factor);
+  uint16_t getHEQNormalizationFactor(struct lepton_driver *driver);
+#endif
+
 #ifdef __cplusplus
 }
 #endif
--- test/test_lepton-agc.c
+++ test/test_lepton-agc.c
@@ -1,17 +1,212 @@
 #include "unity.h"
 #include "lepton-agc.h"
-#include "mock_lepton-flir.h"
+#include "lepton-flir-defs.h"
+#include "lepton-flir.h"
+#include "mock_lepton-vid.h"
+#include "mock_lepton-sys.h"
+#include "mock_lepton-comm-internal.h"
 #include "mock_lepton-communication.h"
+
+struct lepton_driver driver;
 
 void setUp(void)
 {
+  lepton_communication_init_Ignore();
+  lepton_vid_init_Ignore();
+  lepton_sys_init_Ignore();
+  LeptonFLiR_init(LeptonFLiR_ImageStorageMode_80x60_16bpp,
+                  LeptonFLiR_TemperatureMode_Kelvin, &driver);
 }
 
 void tearDown(void)
 {
 }
 
-void test_lepton_agc_NeedToImplement(void)
+void test_setAGCEnabled_null_driver_pointer(void)
 {
-  TEST_IGNORE_MESSAGE("Need to Implement lepton-agc");
+  setAGCEnabled(0,0);
 }
+
+void test_getAGCEnabled_null_driver_pointer(void)
+{
+  getAGCEnabled(0);
+}
+
+void test_setAGCPolicy_null_driver_pointer(void)
+{
+  setAGCPolicy(0,0);
+}
+
+void test_getAGCPolicy_null_driver_pointer(void)
+{
+  getAGCPolicy(0);
+}
+
+void test_setHEQScaleFactor_null_driver_pointer(void)
+{
+  setHEQScaleFactor(0,0);
+}
+
+void test_getHEQScaleFactor_null_driver_pointer(void)
+{
+  getHEQScaleFactor(0);
+}
+
+void test_setAGCCalcEnabled_null_driver_pointer(void)
+{
+  setAGCCalcEnabled(0,0);
+}
+
+void test_getAGCCalcEnabled_null_driver_pointer(void)
+{
+  getAGCCalcEnabled(0);
+}
+
+void test_setHistogramRegion_null_driver_pointer(void)
+{
+  LEP_AGC_HISTOGRAM_ROI region;
+  setHistogramRegion(0, &region);
+}
+
+void test_getHistogramRegion_null_driver_pointer(void)
+{
+  LEP_AGC_HISTOGRAM_ROI region;
+  getHistogramRegion(0,&region);
+}
+
+void test_getHistogramStatistics_null_driver_pointer(void)
+{
+  LEP_AGC_HISTOGRAM_STATISTICS statistics;
+  getHistogramStatistics(0,&statistics);
+}
+
+void test_setHistogramClipPercent_null_driver_pointer(void)
+{
+  setHistogramClipPercent(0,0);
+}
+
+void test_getHistogramClipPercent_null_driver_pointer(void)
+{
+  getHistogramClipPercent(0);
+}
+
+void test_setHistogramTailSize_null_driver_pointer(void)
+{
+  setHistogramTailSize(0,0);
+}
+
+void test_getHistogramTailSize_null_driver_pointer(void)
+{
+  getHistogramTailSize(0);
+}
+
+void test_setLinearMaxGain_null_driver_pointer(void)
+{
+  setLinearMaxGain(0,0);
+}
+
+void test_getLinearMaxGain_null_driver_pointer(void)
+{
+  getLinearMaxGain(0);
+}
+
+void test_setLinearMidpoint_null_driver_pointer(void)
+{
+  setLinearMidpoint(0,0);
+}
+
+void test_getLinearMidpoint_null_driver_pointer(void)
+{
+  getLinearMidpoint(0);
+}
+
+void test_setLinearDampeningFactor_null_driver_pointer(void)
+{
+  setLinearDampeningFactor(0,0);
+}
+
+void test_getLinearDampeningFactor_null_driver_pointer(void)
+{
+  getLinearDampeningFactor(0);
+}
+
+void test_setHEQDampeningFactor_null_driver_pointer(void)
+{
+  setHEQDampeningFactor(0,0);
+}
+
+void test_getHEQDampeningFactor_null_driver_pointer(void)
+{
+  getHEQDampeningFactor(0);
+}
+
+void test_setHEQMaxGain_null_driver_pointer(void)
+{
+  setHEQMaxGain(0,0);
+}
+
+void test_getHEQMaxGain_null_driver_pointer(void)
+{
+  getHEQMaxGain(0);
+}
+
+void test_setHEQClipLimitHigh_null_driver_pointer(void)
+{
+  setHEQClipLimitHigh(0,0);
+}
+
+void test_getHEQClipLimitHigh_null_driver_pointer(void)
+{
+  getHEQClipLimitHigh(0);
+}
+
+void test_setHEQClipLimitLow_null_driver_pointer(void)
+{
+  setHEQClipLimitLow(0,0);
+}
+
+void test_getHEQClipLimitLow_null_driver_pointer(void)
+{
+  getHEQClipLimitLow(0);
+}
+
+void test_setHEQBinExtension_null_driver_pointer(void)
+{
+  setHEQBinExtension(0,0);
+}
+
+void test_getHEQBinExtension_null_driver_pointer(void)
+{
+  getHEQBinExtension(0);
+}
+
+void test_setHEQMidpoint_null_driver_pointer(void)
+{
+  setHEQMidpoint(0,0);
+}
+
+void test_getHEQMidpoint_null_driver_pointer(void)
+{
+  getHEQMidpoint(0);
+}
+
+void test_setHEQEmptyCounts_null_driver_pointer(void)
+{
+  setHEQEmptyCounts(0,0);
+}
+
+void test_getHEQEmptyCounts_null_driver_pointer(void)
+{
+  getHEQEmptyCounts(0);
+}
+
+void test_setHEQNormalizationFactor_null_driver_pointer(void)
+{
+  setHEQNormalizationFactor(0,0);
+}
+
+void test_getHEQNormalizationFactor_null_driver_pointer(void)
+{
+  getHEQNormalizationFactor(0);
+}
+
