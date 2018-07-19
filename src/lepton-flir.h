ec6e5d5dd567ff145d48ead4e765a76f0d820b0f
Nick Nerovny <nikolay.nerovniy@gmail.com>
2018-07-19 13:52:23 +0300

    Library configuration moved to different file

--- src/lepton-flir.h
+++ src/lepton-flir.h
@@ -32,35 +32,8 @@
 extern "C" {
 #endif
 
-// Library Setup
-
-#ifndef TEST
-// Uncomment this define if wanting to exclude extended i2c functions from compilation.
-#define LEPFLIR_EXCLUDE_EXT_I2C_FUNCS   1
-
-// Uncomment this define if wanting to exclude SYS functions from compilation.
-// #define LEPFLIR_EXCLUDE_SYS_FUNCS   1
-
-// Uncomment this define if wanting to exclude VID functions from compilation.
-#define LEPFLIR_EXCLUDE_VID_FUNCS   1
-
-// Uncomment this define if wanting to exclude AGC functions from compilation.
-// #define LEPFLIR_EXCLUDE_AGC_FUNCS   1
-
-// Uncomment this define if wanting to exclude misc. functions from compilation.
-#define LEPFLIR_EXCLUDE_MISC_FUNCS   1
-
-// Explicitly suppress warning about LEPFLIR_EXCLUDE_MISC_FUNCS
-#define LEPFLIR_EXCLUDE_MISC_FUNCS_SUPPRESS_WARNING 1
-
-// Uncomment this define if wanting to exclude image statistics functions from compilation.
-#define LEPFLIR_EXCLUDE_IMAGE_FUNCS   1
-
-// Uncomment this define to enable debug output functions.
-// #define LEPFLIR_ENABLE_DEBUG_OUTPUT     1
-
-/* Do not use locking instruction in case of single threading */
-// #define LEPFLIR_EXCLUDE_LOCKING
+/* User configuration file for this library should be somewhere in search path */
+#include <lepton-config.h>
 
 #ifndef LEPFLIR_EXCLUDE_MISC_FUNCS
 #ifdef LEPFLIR_EXCLUDE_IMAGE_FUNCS
@@ -74,8 +47,6 @@ extern "C" {
 #pragma message "It is recommended to undefine LEPFLIR_EXCLUDE_MISC_FUNCS"
 #endif
 #endif
-#endif
-
 #endif
 
 #include "lepton-flir-defs.h"
---
+++ test/support/lepton-config.h
@@ -0,0 +1,38 @@
+#ifndef _LEPTON_CONFIG_H
+#define _LEPTON_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Uncomment this define if wanting to exclude extended i2c functions from compilation.
+// #define LEPFLIR_EXCLUDE_EXT_I2C_FUNCS   1
+
+// Uncomment this define if wanting to exclude SYS functions from compilation.
+// #define LEPFLIR_EXCLUDE_SYS_FUNCS   1
+
+// Uncomment this define if wanting to exclude VID functions from compilation.
+// #define LEPFLIR_EXCLUDE_VID_FUNCS   1
+
+// Uncomment this define if wanting to exclude AGC functions from compilation.
+// #define LEPFLIR_EXCLUDE_AGC_FUNCS   1
+
+// Uncomment this define if wanting to exclude misc. functions from compilation.
+// #define LEPFLIR_EXCLUDE_MISC_FUNCS   1
+
+// Explicitly suppress warning about LEPFLIR_EXCLUDE_MISC_FUNCS
+// #define LEPFLIR_EXCLUDE_MISC_FUNCS_SUPPRESS_WARNING 1
+
+// Uncomment this define if wanting to exclude image statistics functions from compilation.
+// #define LEPFLIR_EXCLUDE_IMAGE_FUNCS   1
+
+/* Do not use locking instruction in case of single threading */
+// #define LEPFLIR_EXCLUDE_LOCKING
+
+// Uncomment this define to enable debug output functions.
+// #define LEPFLIR_ENABLE_DEBUG_OUTPUT     1
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _LEPTON_CONFIG_H */
